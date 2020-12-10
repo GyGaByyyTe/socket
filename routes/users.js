87b2a29d19651236da187fa128ee41a12fb1a33c
Лебедев Андрей Владимирович <lebedev.an.vl@sberbank.ru>
2020-12-10 22:38:36 +0300

    add db

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
+const db = require('../queries');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+router.get('/', function (req, res) {
+  db.getUsers(req, res);
+})
+router.get('/:id', function (req, res) {
+  db.getUserById(req, res);
+})
+router.post('/', function (req, res) {
+  db.createUser(req, res);
+})
+router.put('/:id', function (req, res) {
+  db.updateUser(req, res);
+})
+router.delete('/:id', function (req, res) {
+  db.deleteUser(req, res);
+})
 
 module.exports = router;
